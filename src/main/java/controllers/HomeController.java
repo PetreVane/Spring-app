21fe393bce58058c916d4c7ae7ad10c77bf0e301
Petre Vane <petre.vane@gmail.com>
2021-02-25 12:00:13 +0100

    first commit --> project setup succesfully

---
+++ src/main/java/controllers/HomeController.java
@@ -0,0 +1,20 @@
+package controllers;
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+@RequestMapping("/")
+public class HomeController {
+
+    @RequestMapping(method = RequestMethod.GET)
+    public String getHomePage(ModelMap modelMap) {
+        modelMap.addAttribute("message", "Welcome to Home Page!");
+        return "home-page";
+    }
+
+}
---
+++ src/main/webapp/WEB-INF/view/home-page.jsp
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <title>
+        Home Page
+    </title>
+
+
+</head>
+
+<body>
+    <h2> ${message}</h2>
+</body>
+
+</html>
